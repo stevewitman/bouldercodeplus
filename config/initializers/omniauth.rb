4aaab30ec5d77777af31c85a67fc6c9e0a1cac06
Steve Witman <stevewitman@gmail.com>
2015-08-02 20:40:48 -0600

    try addressing CA certs in initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
+  provider :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET'], {
+    client_options: { ssl: {
+        ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
+        ca_path: "/etc/ssl/certs"
+    }}
+  }
 end
