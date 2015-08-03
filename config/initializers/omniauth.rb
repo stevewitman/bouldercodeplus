a3bc2bf7b4441a3c98137ee15799d553f4868437
Steve Witman <stevewitman@gmail.com>
2015-08-03 00:46:18 -0600

    take back out

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET'], {
-    client_options: { ssl: {
-        ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
-        ca_path: "/etc/ssl/certs"
-    }}
-  }
+  provider :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
 end
