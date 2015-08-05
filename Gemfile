20bd98af90a2aea74dc51a1d6151f028d8cc6fcd
Steve Witman <stevewitman@gmail.com>
2015-08-04 22:41:36 -0600

    disp some user info, try writing to Firebase on controller

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'omniauth-meetup'
 
+gem 'firebase'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   def index
+    puts "@@@@@@@@@@@@@@@@@"
+    pp current_user
 
+    base_uri = 'https://brilliant-fire-2881.firebaseio.com'
+
+    firebase = Firebase::Client.new(base_uri)
+
+    response = firebase.push("todos", { :name => 'Pick the milk', :priority => 1 })
+    puts response
+
+    # response = firebase.delete('/todos', '')
+    # puts response
   end
 end
