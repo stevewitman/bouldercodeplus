108662a59fa7d2e63fc2937d656236887e97b76f
Steve Witman <stevewitman@gmail.com>
2015-08-06 22:58:26 -0600

    added some controller methods and begin view for add activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,3 +1,52 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      redirect_to activities_path, notice: 'Activity was successfully created'
+    else
+      render :new
+    end
+  end
+
+  def edit
+      @activity = Activity.find(params[:id])
+  end
+
+  def update
+    activity_params = params.require(:activity).permit(:user_id, :max, :title, :description, :date, :status)
+    if @activity.update(activity_params)
+      redirect_to activity_path(@activity), notice: 'Activity was sucessfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @activity.destroy
+    redirect_to projects_path, notice: 'Project was sucessfully deleted'
+  end
+
+private
+
+  def set_activity
+    begin
+      @activity = Activity.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      raise AccessDenied
+    end
+  end
 
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,11 @@
 class Activity < ActiveRecord::Base
-
+  validates :description, presence: true
+  validate :due_date_in_past, :on => :create
+  # has_many :comments, dependent: :destroy
+  def due_date_in_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "can't be in the past")
+    end
+  end
 
 end
